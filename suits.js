419340539d6ed9282f93e654e01754fae535bb93
Machetta Gregg R <machettagreggr@johndeere.com>
2021-02-08 16:47:39 -0600

    Set up Sequelize

---
+++ suits.js
@@ -0,0 +1,36 @@
+const suits = [
+    {
+        name:'Babe Ruth',
+        team: 'yankees',
+        year: '1965'
+    },
+    {
+        name:'Hank Aaron',
+        team: 'braves',
+        year: '1966'
+    },
+    {
+        name:'Mickey Mantle',
+        team: 'yankees',
+        year: '1967',
+    },
+    {
+        name:'Babe Ruth',
+        team: 'yankees',
+        year: '1965',
+    },
+    {
+        name:'Willie Mays',
+        team: 'giants',
+        year: '1967',
+    },
+    {
+        name:'Barry Bonds',
+        team: 'Giants',
+        year: '1968',
+    }
+];
+
+// always at the bottom - telling node what bits of code to export from this file
+//at the bottom of /models/fruits.js --- always at bottom
+module.exports = suits;
---
+++ users.js
@@ -0,0 +1,20 @@
+const users = [
+    {
+        name:'Alvin',
+        username: 'Alvin1',
+        password: 'suit1',
+    },
+    {
+        name:'Bob',
+        username: 'Bob1',
+        password: 'suit2',
+    },
+    {
+        name:'Cole',
+        username: 'Cole1',
+        password: 'suit3',
+    }
+]
+
+// only thing we will export is users
+module.exports = users;
